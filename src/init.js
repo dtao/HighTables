ac7a045d7687c30afc70c0bd0d77e0709917696f
Dan Tao <daniel.tao@gmail.com>
2013-02-13 20:34:52 -0800

    refactored initialization script (MIGHT have overdone it)

--- src/init.js
+++ src/init.js
@@ -5,87 +5,50 @@ HighTables.charts = {};
 $(document).ready(function() {
   Highcharts.setOptions({
     credits: {
-      enabled: false
+      enabled: HighTables.includeHighchartsLinks
     }
   });
 
-  $(".line-chart").each(function() {
-    HighTables.LineChart.renderTo(this);
-  });
+  var chartConfigs = {
+    LineChart: {
+      "line": {},
+      "spline": { chart: { type: "spline" } },
+      "area": { chart: { type: "area" } },
+      "stack": { chart: { type: "area" }, plotOptions: { area: { stacking: "normal" } } }
+    },
+    BarChart: {
+      "bar": {},
+      "column": { chart: { type: "column" } },
+    },
+    PieChart: {
+      "pie": {}
+    }
+  };
 
-  $(".spline-chart").each(function() {
-    HighTables.LineChart.renderTo(this, {
-      chart: { type: "spline" }
-    });
-  });
-
-  $(".area-chart").each(function() {
-    HighTables.LineChart.renderTo(this, {
-      chart: { type: "area" }
-    });
-  });
-
-  $(".stack-chart").each(function() {
-    HighTables.LineChart.renderTo(this, {
-      chart: { type: "area" },
-      plotOptions: { area: { stacking: "normal" } }
-    });
-  });
-
-  $("table.render-to-line-chart").each(function() {
-    HighTables.LineChart.renderFromTable(this);
-  });
-
-  $("table.render-to-spline-chart").each(function() {
-    HighTables.LineChart.renderFromTable(this, {
-      chart: { type: "spline" }
-    });
-  });
-
-  $("table.render-to-area-chart").each(function() {
-    HighTables.LineChart.renderFromTable(this, {
-      chart: { type: "area" }
-    });
-  });
-
-  $("table.render-to-stack-chart").each(function() {
-    HighTables.LineChart.renderFromTable(this, {
-      chart: { type: "area" },
-      plotOptions: { area: { stacking: "normal" } }
-    });
-  });
-
-  $(".bar-chart").each(function() {
-    HighTables.BarChart.renderTo(this);
-  });
-
-  $(".column-chart").each(function() {
-    HighTables.BarChart.renderTo(this, {
-      chart: { type: "column" }
-    });
-  });
-
-  $("table.render-to-bar-chart").each(function() {
-    HighTables.BarChart.renderFromTable(this);
-  });
-
-  $("table.render-to-column-chart").each(function() {
-    HighTables.BarChart.renderFromTable(this, {
-      chart: { type: "column" }
-    });
-  });
-
-  $(".pie-chart").each(function() {
-    HighTables.PieChart.renderTo(this);
-  });
-
-  $("table.render-to-pie-chart").each(function() {
-    HighTables.PieChart.renderFromTable(this);
-  });
-
-  if (HighTables.includeHighchartsLinks) {
-    Highcharts.setOptions({
-      credits: { enabled: true }
+  function renderCharts(selector, engine, options) {
+    $(selector).each(function() {
+      engine.renderTo(this, options);
     });
   }
+
+  function renderChartsFromTables(selector, engine, options) {
+    $(selector).each(function() {
+      engine.renderFromTable(this, options);
+    })
+  }
+
+  function renderChartFromConfig(engine, config) {
+    for (var chartType in config) {
+      renderCharts("." + chartType + "-chart", engine, config[chartType]);
+      renderChartsFromTables(".render-to-" + chartType + "-table", engine, config[chartType]);
+    }
+  }
+
+  function renderChartsFromConfigs() {
+    for (var engine in chartConfigs) {
+      renderChartFromConfig(HighTables[engine], chartConfigs[engine]);
+    }
+  }
+
+  renderChartsFromConfigs();
 });
