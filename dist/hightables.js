16b90c78ddf2c0800d7bf354cabecc6ca1cc91aa
Dan Tao <daniel.tao@gmail.com>
2013-02-18 08:50:39 -0800

    added API for manually rendering a chart

--- dist/hightables.js
+++ dist/hightables.js
@@ -10,45 +10,71 @@ $(document).ready(function() {
   });
 
   var chartConfigs = {
-    LineChart: {
-      "line": {},
-      "spline": { chart: { type: "spline" } },
-      "area": { chart: { type: "area" } },
-      "stack": { chart: { type: "area" }, plotOptions: { area: { stacking: "normal" } } }
-    },
-    BarChart: {
-      "bar": {},
-      "column": { chart: { type: "column" } },
-    },
-    PieChart: {
-      "pie": {}
-    }
+    "line": { engine: HighTables.LineChart },
+    "spline": { engine: HighTables.LineChart, options: { chart: { type: "spline" } } },
+    "area": { engine: HighTables.LineChart, options: { chart: { type: "area" } } },
+    "stack": { engine: HighTables.LineChart, options: { chart: { type: "area" }, plotOptions: { area: { stacking: "normal" } } } },
+    "bar": { engine: HighTables.BarChart },
+    "column": { engine: HighTables.BarChart, options: { chart: { type: "column" } } },
+    "pie": { engine: HighTables.PieChart }
   };
 
-  function renderCharts(selector, engine, options) {
-    $(selector).each(function() {
+  function renderCharts(chartType) {
+    var engine  = chartConfigs[chartType].engine;
+    var options = chartConfigs[chartType].options;
+    $("." + chartType + "-chart").each(function() {
       engine.renderTo(this, options);
     });
   }
 
-  function renderChartsFromTables(selector, engine, options) {
-    $(selector).each(function() {
+  function renderChartsFromTables(chartType) {
+    var engine  = chartConfigs[chartType].engine;
+    var options = chartConfigs[chartType].options;
+    $("table.render-to-" + chartType + "-chart").each(function() {
       engine.renderFromTable(this, options);
     })
   }
 
-  function renderChartFromConfig(engine, config) {
-    for (var chartType in config) {
-      renderCharts("." + chartType + "-chart", engine, config[chartType]);
-      renderChartsFromTables("table.render-to-" + chartType + "-chart", engine, config[chartType]);
+  function renderChartsFromConfigs() {
+    for (var chartType in chartConfigs) {
+      renderCharts(chartType);
+      renderChartsFromTables(chartType);
     }
   }
 
-  function renderChartsFromConfigs() {
-    for (var engine in chartConfigs) {
-      renderChartFromConfig(HighTables[engine], chartConfigs[engine]);
+  function getChartType(chart) {
+    var chartClasses = $(chart).attr("class").split(/\s+/);
+    for (var i = 0; i < chartClasses.length; ++i) {
+      if (chartClasses[i].match(/^(?:line|spline|area|stack|bar|column|pie)-chart$/)) {
+        return chartClasses[i].replace(/-chart$/g, "");
+      }
     }
   }
+
+  function getChartTypeFromTable(table) {
+    var chartClasses = $(table).attr("class").split(/\s+/);
+    for (var i = 0; i < chartClasses.length; ++i) {
+      if (chartClasses[i].match(/^render-to-(?:line|spline|area|stack|bar|column|pie)-chart$/)) {
+        return chartClasses[i].replace(/^render-to-/g, "").replace(/-chart$/g, "");
+      }
+    }
+  }
+
+  HighTables.renderCharts = renderChartsFromConfigs;
+
+  HighTables.renderChart = function(chart) {
+    var chartType = getChartType(chart);
+    var engine    = chartConfigs[chartType].engine;
+    var options   = chartConfigs[chartType].options;
+    engine.renderTo(chart, options);
+  };
+
+  HighTables.renderChartFromTable = function(table) {
+    var chartType = getChartTypeFromTable(chart);
+    var engine    = chartConfigs[chartType].engine;
+    var options   = chartConfigs[chartType].options;
+    engine.renderFromTable(table, options);
+  };
 
   renderChartsFromConfigs();
 });
